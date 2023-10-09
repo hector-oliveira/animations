43f634accbccad4df0e264606118be91c35453b5
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-09 11:10:33 -0300

    refactor: Cleaning up the Eye component code

--- src/animations/Eye.tsx
+++ src/animations/Eye.tsx
@@ -1,35 +1,11 @@
-import 
-React,{
-  useState,
-  useRef,
-  useEffect,
-} from 'react';
-
+import React from 'react';
 import { TouchableOpacity } from 'react-native';
-
 import LottieView from 'lottie-react-native';
 import eye from '@assets/eye.json';
+import { useEye } from '@hooks/useEye';
 
 export const Eye = () => {
-  const [isSave, setIsSave] = useState(true);
-  const animationRef = useRef<LottieView>(null);
-  const isFirstRunRef = useRef(true);
-
-  const handleClickAnimation = () => setIsSave(!isSave);
-
-  useEffect(() => {
-    if (isFirstRunRef.current) {
-      const startFrame = isSave ? 8 : 0;
-      const endFrame = isSave ? 8 : 0;
-      animationRef.current?.play(startFrame, endFrame);
-      isFirstRunRef.current = false;
-    } else {
-      const startFrame = isSave ? 0 : 8;
-      const endFrame = isSave ? 8 : 0;
-      animationRef.current?.play(startFrame, endFrame);
-    }
-  }, [isSave]);
-
+  const { animationRef, handleClickAnimation } = useEye();
   return (
     <TouchableOpacity onPress={handleClickAnimation}>
       <LottieView
