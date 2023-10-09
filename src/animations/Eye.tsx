13bb7a3dda11df1db7ab2d90cdd8b3b7834f01a5
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-09 10:39:00 -0300

    refactor: Improving the structure and function names of Eye component

--- src/animations/Eye.tsx
+++ src/animations/Eye.tsx
@@ -10,43 +10,36 @@ import { TouchableOpacity } from 'react-native';
 import LottieView from 'lottie-react-native';
 import eye from '@assets/eye.json';
 
-
 export const Eye = () => {
-  const [save, setSave] = useState(true);
-  const animation = useRef<LottieView>(null);
-  const isFirstRun = useRef(true);
-  
-  const handleClickAnimation = () => setSave(!save);
+  const [isSave, setIsSave] = useState(true);
+  const animationRef = useRef<LottieView>(null);
+  const isFirstRunRef = useRef(true);
+
+  const handleClickAnimation = () => setIsSave(!isSave);
 
   useEffect(() => {
-    if(isFirstRun.current){
-      if(save && animation.current){
-        animation.current?.play(8,8);
-      } else {
-        animation.current?.play(0,0);
-      }
-      
-      isFirstRun.current = false;
-    }else if(save) {
-      animation.current?.play(0,8);
+    if (isFirstRunRef.current) {
+      const startFrame = isSave ? 8 : 0;
+      const endFrame = isSave ? 8 : 0;
+      animationRef.current?.play(startFrame, endFrame);
+      isFirstRunRef.current = false;
     } else {
-      animation.current?.play(8,0);
+      const startFrame = isSave ? 0 : 8;
+      const endFrame = isSave ? 8 : 0;
+      animationRef.current?.play(startFrame, endFrame);
     }
-  }, [save]);
+  }, [isSave]);
 
-  
-  return(
+  return (
     <TouchableOpacity onPress={handleClickAnimation}>
-      <LottieView 
+      <LottieView
         source={eye}
         autoPlay={false}
         loop={false}
         style={{ width: 50, height: 50 }}
         resizeMode='cover'
-        ref={animation}
-        
-        
+        ref={animationRef}
       />
     </TouchableOpacity>
   );
-}
+};
