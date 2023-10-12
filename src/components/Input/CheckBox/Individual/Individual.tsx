92839895bc80695f1a469670a100171c46be0e44
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-12 15:53:40 -0300

    feat: New CheckBox.Individual feats

--- src/components/Input/CheckBox/Individual/Individual.tsx
+++ src/components/Input/CheckBox/Individual/Individual.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { ElementType } from 'react';
 import * as S from './Individual.styles';
 import { CheckBoxProps } from '@interfaces/CheckBox.props';
+import { Feather } from '@expo/vector-icons';
+import { useCheckBox } from '@hooks/useCheckBoxIndividual';
 
 export const Individual = ( {
-  options = [],
+  options,
   onChange
-}: CheckBoxProps ) => {
+}: CheckBoxProps) => {
+  const { isClicked, handleClick } = useCheckBox();
+
   return (
-    <S.Container>
+    <S.Container
+      onPress={handleClick}
+      isClicked={isClicked}
+    >
+      <Feather 
+        name="check-circle"
+        size={20}
+        color={ isClicked ? '#FFFFFF' : '#000000' }
+        backgroundColor={ isClicked ? '#30DD64' : '#FFFFFF' }
+        style={ { borderRadius: 10 } }
+      />
       {options.map((option, index) => 
         <S.Text key={index}>{option?.text}</S.Text>
       )}
