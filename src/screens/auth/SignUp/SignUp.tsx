a6c34e40e5c551105de7ff389931ab652e23a240
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-12 00:26:09 -0300

    feat: Placing search input properties on the home screen

--- src/screens/auth/SignUp/SignUp.tsx
+++ src/screens/auth/SignUp/SignUp.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './SignUp.styles';
-import { Eye } from '@animations/Eye';
 import { Input } from '@components/Input/Input';
+import { useTextInput } from '@hooks/useTextInput';
 
 export const SignUp = () => {
+  const { textInput, handleTextInput } = useTextInput();
   return (
     <S.Container>
       <Input.CheckBox />
       <Input.Password />
       <Input.Radio />
-      <Input.Search />
+      <Input.Search 
+        onChangeText={handleTextInput}
+        value={textInput}
+        placeholder='Pesquise aqui'
+        icon='search'
+      />
       <Input.Text />
     </S.Container>
   );
