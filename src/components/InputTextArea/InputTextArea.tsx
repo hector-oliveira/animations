f4eea1e083b29c89bf526f5624a0afdabab3f5bd
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-12 00:24:15 -0300

    feat: Adding new feats

--- src/components/InputTextArea/InputTextArea.tsx
+++ src/components/InputTextArea/InputTextArea.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import * as S from './InputTextArea.styles';
+import { InputTextProps } from '@interfaces/InputProps';
 
-export const InputTextArea = () => {
+export const InputTextArea = ({ onChangeText, placeholder }: InputTextProps) => {
   
   return (
-    <S.Container>
+    <S.Container
+      onChangeText={onChangeText}
+      placeholder={placeholder}
+    >
     </S.Container>
   );
 };
