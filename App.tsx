3fec7119e1ade63d7e054364cb9abbaad977ff9b
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-16 11:59:41 -0300

    improve: App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { ThemeProvider } from "styled-components";
-import theme from "@themes/tokes.themes"
 import { StatusBar } from "expo-status-bar";
 import { AuthRoutes } from "@routes/auth.routes";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { theme } from "@themes/theme";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={ { flex: 1 } }>
         <StatusBar 
           style="dark" 
           backgroundColor="transparent" 
