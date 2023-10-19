a88da9ad721e5b48d13a56a7d8e3a356ac0d7a42
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-18 23:35:43 -0300

    delete: interface Icon

--- src/global/interfaces/Icon.props.ts
+++
@@ -1,8 +0,0 @@
-import { OpaqueColorValue } from "react-native";
-import { Feather,AntDesign } from "@expo/vector-icons";
-
-export type IconProps = {
-  name: React.ComponentProps<typeof Feather>['name'];
-  size: number;
-  color?: string | OpaqueColorValue;
-};
