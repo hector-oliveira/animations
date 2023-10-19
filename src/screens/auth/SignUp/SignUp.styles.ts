9cddef893297d77ec64054df578624e16dce6c49
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-18 23:37:22 -0300

    feat: Using render props

--- src/screens/auth/SignUp/SignUp.styles.ts
+++ src/screens/auth/SignUp/SignUp.styles.ts
@@ -6,5 +6,7 @@ export const Container = styled.SafeAreaView`
   align-items: center;
   justify-content: center;
   padding: 0 30px;
-  background-color: ${theme.colors.lightColor[800] };
+  background-color: ${ theme.colors.lightColor[700] };
 `;
+
+export const Button = styled.TouchableOpacity``;
--- src/screens/auth/SignUp/SignUp.tsx
+++ src/screens/auth/SignUp/SignUp.tsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import * as S from './SignUp.styles';
 import { Input } from '@components/Input/Input';
-import { theme } from '@themes/theme';
+import { useCheckBox } from '@hooks/useCheckBoxIndividual';
+import { useSignUp } from './useSignUp';
+
 
 export const SignUp = () => {
   const acceptanceOption = [{ text: 'Li e concordo com os termos', id: 1 }];
+  const { isClicked, handleClick } = useCheckBox();
+  const { handleIconRendering } = useSignUp(isClicked);
+
   return (
     <S.Container>
-      <Input.CheckBox.Individual 
-        options={acceptanceOption}
-        name='check-square'
-        size={22}
-        color={ theme.colors.check }
-      />
+      <S.Button
+        onPress={handleClick}
+        isClicked={isClicked}
+      >
+        <Input.CheckBox.Individual 
+          options={acceptanceOption}
+          icon={handleIconRendering}
+        />
+      </S.Button>
+
       <Input.CheckBox.OnlyOptions />
       <Input.CheckBox.MultipleOptions />
       {/* <Input.Password />
