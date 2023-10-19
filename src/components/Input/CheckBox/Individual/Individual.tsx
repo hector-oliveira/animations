a2cfdcb71b50eeb95176147c6b87581cca366d31
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-18 23:33:53 -0300

    feat: Render Props

--- src/components/Input/CheckBox/Individual/Individual.tsx
+++ src/components/Input/CheckBox/Individual/Individual.tsx
@@ -1,28 +1,16 @@
-import React, { ElementType } from 'react';
+import React from 'react';
 import * as S from './Individual.styles';
 import { CheckBoxProps } from '@interfaces/CheckBox.props';
-import { useCheckBox } from '@hooks/useCheckBoxIndividual';
-import { IconProps } from '@interfaces/Icon.props';
-import { Feather } from '@expo/vector-icons';
 
 export const Individual = ( {
   options,
-  name,
-  size = 20,
-  onChange
-}: CheckBoxProps & IconProps) => {
-  const { isClicked, handleClick } = useCheckBox();
-
+  icon,
+  onChange,
+  ...rest
+}: CheckBoxProps) => {
   return (
-    <S.Container
-      onPress={handleClick}
-      isClicked={isClicked}
-    >
-     <Feather 
-        name={name}
-        size={size}
-        color={isClicked ? '#30BB64' : '#000000'}
-     /> 
+    <S.Container>
+     { icon && icon()}
       {options.map((option, index) => 
         <S.Text key={index}>{option?.text}</S.Text>
       )}
