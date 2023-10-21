b213867aaef279518ee546c239ebdfbf5d63ce9f
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-21 12:05:19 -0300

    deleted: Files and folder deleted for improvements

--- src/components/Input/CheckBox/CheckBox.tsx
+++
@@ -1,9 +0,0 @@
-import { Individual }      from './Individual/Individual';
-import { MultipleOptions } from './MultipleOptions/MultipleOptions';
-import { OnlyOptions }     from './OnlyOptions/OnlyOptions';
-
-export const CheckBox = {
-  Individual,
-  MultipleOptions,
-  OnlyOptions,
-};
