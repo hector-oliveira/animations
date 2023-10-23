1cc4fc0f336e51c74c4646a864d9a69d8c1e88ac
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-23 14:33:30 -0300

    move: App for src

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { StatusBar } from "expo-status-bar";
+import { AuthRoutes } from "@routes/auth.routes";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { theme } from "@themes/theme";
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <SafeAreaView style={ { flex: 1 } }>
+        <StatusBar 
+          style="light"
+          backgroundColor={ theme.color.bgContainer} 
+        />
+        <AuthRoutes />
+      </SafeAreaView>
+    </ThemeProvider>
+  );
+}
