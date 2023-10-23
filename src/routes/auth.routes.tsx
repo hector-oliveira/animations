9e26e7db9472d415604e9242ad75eff3ee042853
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-23 14:43:04 -0300

    improvement: code

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator  } from '@react-navigation/native-stack';
+import * as screen from '@screens-auth/auth-screens';
 
-import * as screen from '@screens-auth/auth.screens';
-
-const AuthStack = createNativeStackNavigator();
+const { Navigator, Screen } = createNativeStackNavigator();
 
 export const AuthRoutes = () => {
   return(
     <NavigationContainer>
-      <AuthStack.Navigator
-        screenOptions={{
-          headerShown: false 
-         }}
-      >
-        <AuthStack.Screen
-          name="SignIn"
-          component={screen.SignUp}
-        />
-      </AuthStack.Navigator>
+      <Navigator screenOptions={ { headerShown: false } } >
+        <Screen name="SignIn" component={screen.SignUp} />
+      </Navigator>
     </NavigationContainer>
   );
 }
