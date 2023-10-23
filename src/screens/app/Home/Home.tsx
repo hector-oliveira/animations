a20ad3d654a72e61948dd549c7eea24f2ec1ebf0
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-23 14:46:44 -0300

    feat: New app files

---
+++ src/screens/app/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './Home.styles';
+
+export const Home = () => {
+  return (
+    <S.Container>
+      <S.Text>Home</S.Text>
+    </S.Container>
+  );
+};
