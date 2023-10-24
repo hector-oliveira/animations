e2cde3fa24ec720905a6f15c562cc63a65317392
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-24 15:57:12 -0300

    improvements: new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { ThemeProvider } from "styled-components";
 import { StatusBar } from "expo-status-bar";
-import { AuthRoutes } from "@routes/auth.routes";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { theme } from "@themes/theme";
+// import { AuthRoutes } from "@routes/auth.routes";
+import { AppRoutes } from "@routes/app.routes";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView style={ { flex: 1 } }>
         <StatusBar 
-          style="light"
-          backgroundColor={ theme.color.bgContainer} 
+          style="dark"
+          backgroundColor={ theme.color.light[800]} 
         />
-        <AuthRoutes />
+        <AppRoutes />
       </SafeAreaView>
     </ThemeProvider>
   );
