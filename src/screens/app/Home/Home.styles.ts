e64ce8be3bb363e10e861ccdb28fb805659ffc72
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-24 15:59:38 -0300

    feat: new home

--- src/screens/app/Home/Home.styles.ts
+++ src/screens/app/Home/Home.styles.ts
@@ -4,11 +4,19 @@ import { theme } from '@themes/theme';
 export const Container = styled.View`
   flex: 1;
   width: 100%;
-  justify-content: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
   align-items: center;
+  gap: 10px;
+  margin-top: 20px;
 `;
 
-export const Text = styled.Text`
-  font-size: 24px;
-  color: #000;
+export const Button = styled.TouchableOpacity`
+  width: 85%;
+  height: 70px;
+  border-radius: 8px;
+  background-color: ${theme.color.status_bar};
+  color: ${theme.color.light[900]};
 `;
--- src/screens/app/Home/Home.tsx
+++ src/screens/app/Home/Home.tsx
@@ -1,10 +1,61 @@
 import React from 'react';
 import * as S from './Home.styles';
+import { ButtonProducts } from '@components/all-components';
+import { useNavigation } from '@react-navigation/native';
+import { AppNavigationContextProperty } from '@routes/TypesRoutes/NavigationProps';
+
+const images = [
+  { source: require('@assets/image/home/teste1.png') },
+  { source: require('@assets/image/home/teste2.png') },
+  { source: require('@assets/image/home/teste2.png') },
+  { source: require('@assets/image/home/teste1.png') },
+  { source: require('@assets/image/home/teste1.png') },
+  { source: require('@assets/image/home/teste2.png') },
+  { source: require('@assets/image/home/teste2.png') },
+  { source: require('@assets/image/home/teste1.png') }
+];
 
 export const Home = () => {
+  const navigation = useNavigation<AppNavigationContextProperty>();
+  const handleNavigation = (id: string, img: Object) => navigation.navigate(
+      'details',
+      { id, image: img }
+    )
   return (
     <S.Container>
-      <S.Text>Home</S.Text>
+      <ButtonProducts 
+        source={images[0].source}
+        onPress={() => handleNavigation('imagem 1', images[0].source)}
+      />
+
+      <ButtonProducts 
+        source={images[1].source}
+        onPress={() => handleNavigation('imagem 2', images[1].source)} />
+
+      <ButtonProducts 
+        source={images[2].source}
+        onPress={() => handleNavigation('imagem 3', images[2].source)} />
+
+      <ButtonProducts 
+        source={images[3].source}
+        onPress={() => handleNavigation('imagem 4', images[3].source)} />
+
+      <ButtonProducts 
+        source={images[4].source}
+        onPress={() => handleNavigation('imagem 5', images[4].source)} />
+
+      <ButtonProducts 
+        source={images[5].source}
+        onPress={() => handleNavigation('imagem 6', images[5].source)} />
+
+      <ButtonProducts 
+        source={images[6].source}
+        onPress={() => handleNavigation('imagem 7', images[6].source)} />
+
+      <ButtonProducts 
+        source={images[7].source}
+        onPress={() => handleNavigation('imagem 8', images[7].source)} />
+
     </S.Container>
   );
 };
