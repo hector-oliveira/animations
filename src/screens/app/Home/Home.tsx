d09415c34f488dc33cc8fa598aa2104de47de7ce
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-24 22:21:09 -0300

    refactor: Using hook for improve the code

--- src/screens/app/Home/Home.tsx
+++ src/screens/app/Home/Home.tsx
@@ -1,62 +1,53 @@
 import React from 'react';
 import * as S from './Home.styles';
 import { ButtonProducts } from '@components/all-components';
-import { useNavigation } from '@react-navigation/native';
-import { AppNavigationContextProperty } from '@routes/TypesRoutes/NavigationProps';
-import { ImagePropsBase } from 'react-native';
-
-const images = [
-  { source: require('@assets/image/home/teste1.png') },
-  { source: require('@assets/image/home/teste2.png') },
-  { source: require('@assets/image/home/teste2.png') },
-  { source: require('@assets/image/home/teste1.png') },
-  { source: require('@assets/image/home/teste1.png') },
-  { source: require('@assets/image/home/teste2.png') },
-  { source: require('@assets/image/home/teste2.png') },
-  { source: require('@assets/image/home/teste1.png') }
-];
+import { useDataImage, useScreensNavigation } from './hook/index';
 
 export const Home = () => {
-  const navigation = useNavigation<AppNavigationContextProperty>();
-
-  const handleNavigation = (id: string, image: Object ) => navigation.navigate(
-      'details',
-      { id, image }
-    )
+  const { images } = useDataImage();
+  const { handleNavigation } = useScreensNavigation();
   return (
     <S.Container>
       <ButtonProducts 
         source={images[0].source}
-        onPress={() => handleNavigation('ID = imagem 1', images[0].source)}
+        onPress={() => handleNavigation('Index: 1', 'images[0].source')}
+      />
+      
+      <ButtonProducts 
+        source={images[1].source}
+        onPress={() => handleNavigation('Index: 2', 'images[1].source')}
       />
 
       <ButtonProducts 
-        source={images[1].source}
-        onPress={() => handleNavigation('ID = imagem 2', 'IMAGEM: 2')} />
-
-      <ButtonProducts 
         source={images[2].source}
-        onPress={() => handleNavigation('ID = imagem 3', 'IMAGEM: 3')} />
+        onPress={() => handleNavigation('Index: 3', 'images[2].source')}
+      />
 
       <ButtonProducts 
         source={images[3].source}
-        onPress={() => handleNavigation('ID = imagem 4', 'IMAGEM: 4')} />
+        onPress={() => handleNavigation('Index: 4', 'images[3].source')}
+      />
 
       <ButtonProducts 
         source={images[4].source}
-        onPress={() => handleNavigation('ID = imagem 5', 'IMAGEM: 5')} />
+        onPress={() => handleNavigation('Index: 5', 'images[4].source')}
+      />
 
       <ButtonProducts 
         source={images[5].source}
-        onPress={() => handleNavigation('ID = imagem 6', 'IMAGEM: 6')} />
+        onPress={() => handleNavigation('Index: 6', 'images[5].source')}
+      />
 
       <ButtonProducts 
         source={images[6].source}
-        onPress={() => handleNavigation('ID = imagem 7', 'IMAGEM: 7')} />
+        onPress={() => handleNavigation('Index: 7', 'images[6].source')}
+      />
 
       <ButtonProducts 
         source={images[7].source}
-        onPress={() => handleNavigation('ID = imagem 8', 'IMAGEM: 8')} />
+        onPress={() => handleNavigation('Index: 8', 'images[7].source')}
+      />
+
 
     </S.Container>
   );
