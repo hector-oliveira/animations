c695d4089981d737c7d6627a9b6f2ce5b5c35a55
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-26 21:26:35 -0300

    feat: Data to be used to sign up

---
+++ src/screens/auth/SignUp/data/optionsCheckbox.ts
@@ -0,0 +1,14 @@
+import { OptionsCheckboxProps } from '@components/Input/Checkbox/utils/Checkbox.interface';
+
+export const optionsForCheckbox: OptionsCheckboxProps[] = [
+  { id: 1, label: 'Li e concordo com os termos.' },
+  { id: 2, label: 'Não li nada.' },
+];
+
+export const sideDishes: OptionsCheckboxProps[] = [
+  { id: 1, label: 'Batata' },
+  { id: 2, label: 'Salada' },
+  { id: 3, label: 'Refrigerante' },
+  { id: 4, label: 'Molho' },
+  { id: 5, label: 'Vinagrete' },
+]
