c17d381e27ec9b363f0714d72279e9ca01a4b3c4
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-30 12:57:58 -0300

    feat: Home screen finished

--- src/screens/app/Home/Home.tsx
+++ src/screens/app/Home/Home.tsx
@@ -11,42 +11,42 @@ export const Home = () => {
     <S.Container>
       <ButtonProducts 
         source={images[0].source}
-        onPress={() => handleNavigation('Index: 1', 'images[0].source')}
+        onPress={() => handleNavigation('Detalhes do produto: 1', images[0].source)}
       />
       
       <ButtonProducts 
         source={images[1].source}
-        onPress={() => handleNavigation('Index: 2', 'images[1].source')}
+        onPress={() => handleNavigation('Detalhes do produto: 2', images[1].source)}
       />
 
       <ButtonProducts 
         source={images[2].source}
-        onPress={() => handleNavigation('Index: 3', 'images[2].source')}
+        onPress={() => handleNavigation('Detalhes do produto: 3', images[2].source)}
       />
 
       <ButtonProducts 
         source={images[3].source}
-        onPress={() => handleNavigation('Index: 4', 'images[3].source')}
+        onPress={() => handleNavigation('Detalhes do produto: 4', images[3].source)}
       />
 
       <ButtonProducts 
         source={images[4].source}
-        onPress={() => handleNavigation('Index: 5', 'images[4].source')}
+        onPress={() => handleNavigation('Detalhes do produto: 5', images[4].source)}
       />
 
       <ButtonProducts 
         source={images[5].source}
-        onPress={() => handleNavigation('Index: 6', 'images[5].source')}
+        onPress={() => handleNavigation('Detalhes do produto: 6', images[5].source)}
       />
 
       <ButtonProducts 
         source={images[6].source}
-        onPress={() => handleNavigation('Index: 7', 'images[6].source')}
+        onPress={() => handleNavigation('Detalhes do produto: 7', images[6].source)}
       />
 
       <ButtonProducts 
         source={images[7].source}
-        onPress={() => handleNavigation('Index: 8', 'images[7].source')}
+        onPress={() => handleNavigation('Detalhes do produto: 8', images[7].source)}
       />
 
 
