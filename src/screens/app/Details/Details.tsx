2932ab7612c01d11f5f9b2f9bc571712fe3ff32a
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-30 12:57:03 -0300

    fix: Preview to each image in Details screen

--- src/screens/app/Details/Details.tsx
+++ src/screens/app/Details/Details.tsx
@@ -2,19 +2,20 @@ import React, { useContext } from 'react';
 import * as S from './Details.styles';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AppScreensParamList } from '@routes/TypeParamList';
-import { useRoute } from '@react-navigation/native';
-import { AppRoutesNames } from '@routes/TypeParamList/TypingForNavigation';
 
 type Props = NativeStackScreenProps<AppScreensParamList, 'details'>
 
 export const Details = ({ route }: Props) => {
   const { id, image } = route.params;
-  
+
   return (
     <S.Container>
       <S.Text>{id}</S.Text>
       <S.ImageContainer>
-        <S.Image>{image}</S.Image>
+        
+        <S.Image 
+          source={image}
+        />
       </S.ImageContainer>
     </S.Container>
   );
