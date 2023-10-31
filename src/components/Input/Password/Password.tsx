55b9cfce5712aa0d26a07e8d1f3392ed5d417788
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-31 18:34:28 -0300

    feat: Password screen complete

--- src/components/Input/Password/Password.tsx
+++ src/components/Input/Password/Password.tsx
@@ -3,7 +3,11 @@ import * as S from './Password.styles';
 import { TextInputArea } from '@components/all-components';
 import { FontAwesome } from '@expo/vector-icons';
 import { theme } from '@themes/theme';
-import { useConditionalIconRender, useToggleState } from '@hooks/index';
+import {
+  useConditionalIconRender,
+  useToggleState
+} from '@hooks/index';
+import { Eye } from '@animations/Eye';
 
 
 export const Password = () => {
@@ -13,16 +17,16 @@ export const Password = () => {
     trueIconName: 'eye-slash', 
     falseIconName: 'eye'
   });
+  
   return (
     <S.Container>
       <S.IconContainer>
-        <FontAwesome  name="lock" size={22} color={ theme.color.dark[900] } />
+        <FontAwesome name='lock' size={20} color={ theme.color.dark[800] } />
       </S.IconContainer>
       <TextInputArea 
         secureTextEntry={isToggle}
         autoCapitalize="none"
         placeholder="Senha"
-        placeholderTextColor={ theme.color.dark[900] }
       />
       <S.ButtonIconContainer onPress={handleToggleState}>
         { handleIconRendering() }
