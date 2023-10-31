f1fe0754beb2bde2c29fc4e32282604df9111c0a
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-31 18:37:11 -0300

    feat: InputProps interface

--- src/global/interfaces/InputProps.ts
+++ src/global/interfaces/InputProps.ts
@@ -2,10 +2,7 @@ import { TextInputProps } from "react-native";
 import { Feather } from '@expo/vector-icons';
 
 export type InputTextProps = TextInputProps & {
-  onChangeText: (text: string) => void;
-  placeholder: string
-};
-
-export type InputTypesProps = InputTextProps & {
   icon?: React.ComponentProps<typeof Feather>['name'];
+  placeholder?: string
+  onChangeText?: (text: string) => void;
 };
