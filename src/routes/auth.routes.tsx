bd6b759aaf5c36f4da9f471cdbfd1f9d334444c7
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-31 18:38:37 -0300

    feat: new routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -10,10 +10,11 @@ const { Navigator, Screen } = createNativeStackNavigator<AuthScreensParamList>()
 export const AuthRoutes = () => {
   return(
     <NavigationContainer>
-      <Navigator initialRouteName='signUp'  screenOptions={ { headerShown: false } } >
-        <Screen name='signUp' component={screen.SignUp} />
+      <Navigator initialRouteName='welcome' screenOptions={ { headerShown: false } } >
         <Screen name='login' component={screen.Login} />
         <Screen name='recoverPassword' component={screen.RecoverPassword} />
+        <Screen name='signUp' component={screen.SignUp} />
+        <Screen name='welcome' component={screen.Welcome} />
         <Screen name='app' component={AppRoutes} />
       </Navigator>
     </NavigationContainer>
