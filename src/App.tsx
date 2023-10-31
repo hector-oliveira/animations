e568796a72c5f01989d38fc6d122fa122d9001ee
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-10-31 18:30:57 -0300

    feat: Routes in the main file, App.tsx, defined

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { theme } from "@themes/theme";
 import { AuthRoutes } from "@routes/auth.routes";
-import { AppRoutes } from "@routes/app.routes";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView style={ { flex: 1 } }>
         <StatusBar 
-          style="dark"
-          backgroundColor={ theme.color.light[800]} 
+          style="light"
+          backgroundColor={ theme.colors.status_bar} 
         />
-        <AppRoutes />
-        {/* <AuthRoutes /> */}
+        <AuthRoutes />
       </SafeAreaView>
     </ThemeProvider>
   );
