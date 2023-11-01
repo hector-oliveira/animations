602dd84ad60036b32103b2b09f9736bff8105109
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-11-01 14:46:10 -0300

    improve: useDataImage

--- src/screens/app/Home/hook/useDataImage.ts
+++ src/screens/app/Home/hook/useDataImage.ts
@@ -1,13 +1,45 @@
 export const useDataImage = () => {
   const images = [
-    { source: require('@assets/image/home/teste1.png') },
-    { source: require('@assets/image/home/teste2.png') },
-    { source: require('@assets/image/home/teste2.png') },
-    { source: require('@assets/image/home/teste1.png') },
-    { source: require('@assets/image/home/teste1.png') },
-    { source: require('@assets/image/home/teste2.png') },
-    { source: require('@assets/image/home/teste2.png') },
-    { source: require('@assets/image/home/teste1.png') }
+    { 
+      source: require('@assets/image/home/minions.png'),
+      description: '1ª imagem de um minions'
+    },
+
+    { 
+      source: require('@assets/image/home/hamburger.png'),
+      description: '1ª imagem de um hambúrguer'
+    },
+
+    { 
+      source: require('@assets/image/home/milk-shake.png'),
+      description: '1ª imagem de um milk-shake'
+    },
+
+    { 
+      source: require('@assets/image/home/bandeiras.png'),
+      description: '1ª imagem de uma bandeira'
+    },
+
+    { 
+      source: require('@assets/image/home/hamburger.png'),
+      description: '2ª imagem de um hambúrguer'
+    },
+
+    { 
+      source: require('@assets/image/home/minions.png'),
+      description: '2ª imagem de um minions'
+    },
+
+    { 
+      source: require('@assets/image/home/milk-shake.png'),
+      description: '2ª imagem de um shake'
+    },
+
+    { 
+      source: require('@assets/image/home/bandeiras.png'),
+      description: '2ª imagem de uma bandeira'
+    }
+
   ];
   
   return { images };
