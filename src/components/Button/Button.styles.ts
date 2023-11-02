5852e49f8d042ba0238325a1c95ab6c2ee0912c0
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-11-02 19:30:55 -0300

    styles: styled component - Feather

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components/native';
 import { theme } from '@themes/theme';
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.TouchableOpacity`
   height: 50px;
   width: 90%;
+  display: flex;
+  flex-direction: row-reverse;
   justify-content: center;
   align-items: center;
+  gap: 12px;
   border-radius: 35px;
   background-color: ${ theme.colors.social };
 `;
@@ -15,3 +19,5 @@ export const Text = styled.Text`
   text-transform: uppercase;
   color: ${ theme.color.light[800] };
 `;
+
+export const Icon = styled(Feather)``;
