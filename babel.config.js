9882986d4086a59e5ac853ffb68720396f37eee6
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-11-10 14:26:30 -0300

    chore: Babe.config and tsconfig.json

--- babel.config.js
+++ babel.config.js
@@ -7,15 +7,15 @@ module.exports = function(api) {
         root: ['./'],
         alias: {
           '@animations': './src/animations',
+          '@assets'    : './src/assets',
           '@components': './src/components',
+          '@data'      : './src/data',
+          '@hooks'     : './src/hooks',
+          '@interfaces': './src/interfaces',
+          '@routes'    : './src/routes',
           '@app'       : './src/screens/app',
           '@auth'      : './src/screens/auth',
-          '@hooks'     : './src/hooks',
-          '@routes'    : './src/routes',
-          '@data'      : './src/global/data',
-          '@interfaces': './src/global/interfaces',
           '@themes'    : './src/themes',
-          '@assets'    : './src/assets',
         }
       }]
     ]
