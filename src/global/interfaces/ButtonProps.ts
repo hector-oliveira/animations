cd1af625e526041efcaccc20ff1b534c5bb5cd30
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-11-10 14:31:42 -0300

    improve: Deleted files for better organization

--- src/global/interfaces/ButtonProps.ts
+++
@@ -1,8 +0,0 @@
-import { TouchableOpacityProps } from "react-native";
-import { Feather } from "@expo/vector-icons";
-
-export type ButtonProps = TouchableOpacityProps & {
-  placeholder: string,
-  icon?: React.ComponentProps<typeof Feather>['name'],
-  onPress?: (text: string) => void
-}
--- src/global/interfaces/InputProps.ts
+++
@@ -1,8 +0,0 @@
-import { TextInputProps } from "react-native";
-import { Feather } from '@expo/vector-icons';
-
-export type InputTextProps = TextInputProps & {
-  icon?: React.ComponentProps<typeof Feather>['name'];
-  placeholder?: string
-  onChangeText?: (text: string) => void;
-};
