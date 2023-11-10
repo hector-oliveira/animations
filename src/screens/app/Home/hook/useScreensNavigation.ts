ade21b9534c2b869d0624bd7fbc683c9d45dc08d
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-11-10 12:10:24 -0300

    improve: function: HandleNavigation

--- src/screens/app/Home/hook/useScreensNavigation.ts
+++ src/screens/app/Home/hook/useScreensNavigation.ts
@@ -1,11 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
+import { AppScreensParamList } from "@routes/TypeParamList";
 import { AppRoutesNames } from "@routes/TypeParamList/TypingForNavigation";
 
 export const useScreensNavigation = () => {
   const navigation = useNavigation<AppRoutesNames>();
 
-  const handleNavigation = (image: string, description: string) => {
-    navigation.navigate('details', { image, description })
+  const handleNavigation = ({ description, image, price }: AppScreensParamList['details']) => {
+    navigation.navigate('details', { image, description, price });
   }
   
   return { handleNavigation }
