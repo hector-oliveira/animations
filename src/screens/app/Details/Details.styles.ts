0948763f2028136220cfdf1cdbc56ac0948be1b1
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-11-10 12:06:13 -0300

    styles: Creating a styled component and improving the responsiveness of some styled components

--- src/screens/app/Details/Details.styles.ts
+++ src/screens/app/Details/Details.styles.ts
@@ -4,30 +4,35 @@ import { theme } from '@themes/theme';
 export const Container = styled.View`
   flex: 1;
   width: 100%;
-  justify-content: center;
-  align-items: center;
 `;
 
-export const Text = styled.Text`
-  font-size: 24px;
+export const Description = styled.Text`
+  font-size: 18px;
+  opacity: 0.5;
   color: #000;
+  width: 90%;
+  text-align: center;
+  align-self: center;
 `;
 
 export const ImageContainer = styled.SafeAreaView`
-  width: 95%;
+  width: 100%;
   height: 300px;
-  border: 1px solid ${ theme.color.light[800] };
-  border-radius: 8px;
   padding: 8px 0px;
-
-  border: 1px solid ${ theme.color.bgContainer };
+  align-items: center;
+  justify-content: center;
 `;
 
-export const Image = styled.ImageBackground.attrs({
-  resizeMode: 'contain',
-})`
-  height: 100%;
-  width: 100%;
-  border-radius: 8px;
+export const Image = styled.ImageBackground.attrs({ resizeMode: 'contain' })`
+  height: 98%;
+  width: 98%;
+`;
+
+export const Price = styled.Text`
+  font-size: 24px;
+  color: #000;
   text-align: center;
+  align-self: flex-end;
+  margin-top: 40px;
+  margin-right: 20px;
 `;
