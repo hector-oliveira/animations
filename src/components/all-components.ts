9c0b64f03ec098bca848a2373b66afb351a2baf3
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-11-10 12:01:03 -0300

    feat: New component for export

--- src/components/all-components.ts
+++ src/components/all-components.ts
@@ -1,3 +1,4 @@
+export { Button } from './Button/Button';
 export { Header } from './Header/Header';
 export { ProductsButton } from './ProductsButton/ProductsButton';
 export { TextInputArea } from './TextInputArea/TextInputArea';
