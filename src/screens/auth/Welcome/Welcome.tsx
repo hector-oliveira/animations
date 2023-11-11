53420ef391952bbf231499180ce1ebb0b19e1dde
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-11-11 11:45:36 -0300

    improve: Navigation functions

--- src/screens/auth/Welcome/Welcome.tsx
+++ src/screens/auth/Welcome/Welcome.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import * as S from './Welcome.styles';
 import { Button } from '@components/Button/Button';
-import { AuthRoutesNames } from '@routes/TypeParamList/TypingForNavigation';
-import { useNavigation } from '@react-navigation/native';
+import { useChooseTheScreenToNavigate } from './hooks/useChooseTheScreenToNavigate';
 
 export const Welcome = () => {
-  const navigation = useNavigation<AuthRoutesNames>();
-  const handleNavigationToLogin = () => navigation.navigate('login');
-  const handleNavigationToSignUp = () => navigation.navigate('signUp');
-  const handleNavigationToApp = () => navigation.navigate('app');
-
+  const {
+    handleNavigationToApp,
+    handleNavigationToLogin,
+    handleNavigationToSignUp
+  } = useChooseTheScreenToNavigate();
+  
   return (
     <S.Container>
       <S.ImageContainer>
